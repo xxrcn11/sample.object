a4702293dd6091d554a5ad01a42c2004c2cbc639
xxrcn11 <xxrcn11@gmail.com>
2020-06-22 09:14:58 +0900

    예 추가

--- src/main/java/com/bt/example/ObjectApplication.java
+++ src/main/java/com/bt/example/ObjectApplication.java
@@ -1,19 +1,14 @@
 package com.bt.example;
 
-import java.time.DayOfWeek;
 import java.time.Duration;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.Period;
 import java.util.Arrays;
-import java.util.List;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.bt.example.conditions.DayOfWeekFeeCondition;
-import com.bt.example.conditions.DurationFeeCondition;
+import com.bt.example.conditions.FiexedFeeCondition;
 import com.bt.example.money.Money;
 
 @SpringBootApplication
@@ -25,43 +20,68 @@ public class ObjectApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-	
-		FeeRule dayOfWeekRule1 = new FeeRule(
-	    		new DayOfWeekFeeCondition(DayOfWeek.MONDAY),
-	    		new FeeDuration(Duration.ofSeconds(10), Money.wons(38)) ); 
-	    
-	    FeeRule dayOfWeekRule2 = new FeeRule(
-	    		new DayOfWeekFeeCondition(DayOfWeek.TUESDAY),
-	    		new FeeDuration(Duration.ofSeconds(10), Money.wons(38)) );
-	    
-	    FeeRule dayOfWeekRule3 = new FeeRule(
-	    		new DayOfWeekFeeCondition(DayOfWeek.WEDNESDAY),
-	    		new FeeDuration(Duration.ofSeconds(10), Money.wons(38)) );
-	    
-	    FeeRule dayOfWeekRule4 = new FeeRule(
-	    		new DayOfWeekFeeCondition(DayOfWeek.THURSDAY),
-	    		new FeeDuration(Duration.ofSeconds(10), Money.wons(38)) );
-	    
-	    FeeRule dayOfWeekRule5 = new FeeRule(
-	    		new DayOfWeekFeeCondition(DayOfWeek.FRIDAY),
-	    		new FeeDuration(Duration.ofSeconds(10), Money.wons(38)) );
-	    
-	    FeeRule dayOfWeekRule6 = new FeeRule(
-	    		new DayOfWeekFeeCondition(DayOfWeek.SATURDAY),
-	    		new FeeDuration(Duration.ofSeconds(10), Money.wons(19)) );
-	    
-	    FeeRule dayOfWeekRule7 = new FeeRule(
-	    		new DayOfWeekFeeCondition(DayOfWeek.SUNDAY),
-	    		new FeeDuration(Duration.ofSeconds(10), Money.wons(19)) );
-	    
-	    
-	    List<FeeRule> dayOfWeekRules = Arrays.asList(dayOfWeekRule1, dayOfWeekRule2, dayOfWeekRule3, dayOfWeekRule4,
-	    		dayOfWeekRule5, dayOfWeekRule6, dayOfWeekRule7);
-	    
-	    
-	    Phone phone1 = new Phone(new BasicRatePolicy(dayOfWeekRules));
-	    phone1.call(new Call(LocalDateTime.of(2020,  6, 19, 23, 59, 0), LocalDateTime.of(2020,  6, 20, 0, 1, 0)) );
+		FeeRule fixedRule1 = new FeeRule(
+	    		new FiexedFeeCondition(),
+	    		new FeeDuration(Duration.ofSeconds(10), Money.wons(18)) );
+		
+	    Phone phone1 = new Phone(new BasicRatePolicy(Arrays.asList(fixedRule1)));
+	    phone1.call(
+	    		new Call(LocalDateTime.of(2020,  6, 19, 18, 59, 0), 
+	    				 LocalDateTime.of(2020,  6, 20, 19, 1, 0)) 
+	    		);
 	    System.out.println(phone1.calculateFee());
+		
+//		FeeRule timeOfDayRuel1 = new FeeRule(
+//	    		new TimeOfDayFeeCondition( LocalTime.of(0, 0, 0), LocalTime.of(19, 0, 0)  ),
+//	    		new FeeDuration(Duration.ofSeconds(10), Money.wons(18)) );
+//		
+//		FeeRule timeOfDayRuel2 = new FeeRule(
+//	    		new TimeOfDayFeeCondition( LocalTime.of(19, 0, 0), LocalTime.of(23, 59, 59)  ),
+//	    		new FeeDuration(Duration.ofSeconds(10), Money.wons(15)) );
+//	    
+//	    Phone phone1 = new Phone(new BasicRatePolicy(Arrays.asList(timeOfDayRuel1, timeOfDayRuel2)));
+//	    phone1.call(
+//	    		new Call(LocalDateTime.of(2020,  6, 19, 18, 59, 0), 
+//	    				 LocalDateTime.of(2020,  6, 20, 19, 1, 0)) 
+//	    		);
+//	    System.out.println(phone1.calculateFee());
+	
+//		FeeRule dayOfWeekRule1 = new FeeRule(
+//	    		new DayOfWeekFeeCondition(DayOfWeek.MONDAY),
+//	    		new FeeDuration(Duration.ofSeconds(10), Money.wons(38)) ); 
+//	    
+//	    FeeRule dayOfWeekRule2 = new FeeRule(
+//	    		new DayOfWeekFeeCondition(DayOfWeek.TUESDAY),
+//	    		new FeeDuration(Duration.ofSeconds(10), Money.wons(38)) );
+//	    
+//	    FeeRule dayOfWeekRule3 = new FeeRule(
+//	    		new DayOfWeekFeeCondition(DayOfWeek.WEDNESDAY),
+//	    		new FeeDuration(Duration.ofSeconds(10), Money.wons(38)) );
+//	    
+//	    FeeRule dayOfWeekRule4 = new FeeRule(
+//	    		new DayOfWeekFeeCondition(DayOfWeek.THURSDAY),
+//	    		new FeeDuration(Duration.ofSeconds(10), Money.wons(38)) );
+//	    
+//	    FeeRule dayOfWeekRule5 = new FeeRule(
+//	    		new DayOfWeekFeeCondition(DayOfWeek.FRIDAY),
+//	    		new FeeDuration(Duration.ofSeconds(10), Money.wons(38)) );
+//	    
+//	    FeeRule dayOfWeekRule6 = new FeeRule(
+//	    		new DayOfWeekFeeCondition(DayOfWeek.SATURDAY),
+//	    		new FeeDuration(Duration.ofSeconds(10), Money.wons(19)) );
+//	    
+//	    FeeRule dayOfWeekRule7 = new FeeRule(
+//	    		new DayOfWeekFeeCondition(DayOfWeek.SUNDAY),
+//	    		new FeeDuration(Duration.ofSeconds(10), Money.wons(19)) );
+//	    
+//	    
+//	    List<FeeRule> dayOfWeekRules = Arrays.asList(dayOfWeekRule1, dayOfWeekRule2, dayOfWeekRule3, dayOfWeekRule4,
+//	    		dayOfWeekRule5, dayOfWeekRule6, dayOfWeekRule7);
+//	    
+//	    
+//	    Phone phone1 = new Phone(new BasicRatePolicy(dayOfWeekRules));
+//	    phone1.call(new Call(LocalDateTime.of(2020,  6, 19, 23, 59, 0), LocalDateTime.of(2020,  6, 20, 0, 1, 0)) );
+//	    System.out.println(phone1.calculateFee());
 	    
 	    
 
